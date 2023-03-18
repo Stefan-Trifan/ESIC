73c666f681e1028199250b85ffc586f73e0ab48f
dieger75 <dieger75@hotmail.com>
2023-03-18 05:41:32 +0100

    Version Final

--- assets/js/info-typ.js
+++ assets/js/info-typ.js
@@ -16,12 +16,7 @@ var utmAux = window.location.href
 lang_html = document.querySelector('html').lang
 lang = lang_html.toLowerCase().trim().slice(0, 2);
 cod_ga = document.querySelector('meta#data-programs')
-name_ga = document.querySelector('h1')
-if (document.querySelector('h1 + h2')){
-    subtitle = document.querySelector('h1 + h2')
-}
-brochure = document.querySelectorAll('a.clickinfo')
-url_pay = document.querySelectorAll('a.clickadmi')
+
 // holiday = document.querySelectorAll('#holiday')
 //url_pasarela = "https://professionalprogramsmit.com/gateway/" + lang + '/XXX/runway/order'
 
@@ -33,29 +28,36 @@ fetch('../../assets/js/csvjson.json')
         //entramos a cada obj(objeto) del arreglo
         try {
             data.forEach(obj => {
-                console.log(obj)
-                //comparámos el código GA para inyectar valores a la landing
+                // console.log(obj)
+                //comparámos el código GA para inyectar valores a la landingname_ga = document.querySelector('h1')
+
                 if (obj.cod_ga.toLowerCase().trim() === cod_ga.dataset.codga.toLowerCase()) {
-                    name_ga.innerText = obj.program
-                    brochure.innerText = obj.brochure
-                    if (document.querySelector('h1 + h2')){
+
+                    if (document.querySelector('#data-subt')) {
+                        subtitle = document.querySelector('#data-subt')
                         subtitle.innerText = obj.subtitle
                     }
-                    url_pay.forEach(element => {
-                        /*================================*/
-                        /*	SEGUIMIENTO DE LAS UTMS
-                        /*================================*/
-                        if (utmAux.includes('?utm')) {
-                            var utmAdded = utmAux.split('?utm_')[1]
-                            // element.href = url_pasarela.replace('XXX', obj.cod_pay) + '?utm_' + utmAdded
-                            element.href = obj.url_pay + '?utm_' + utmAdded
-                            
-                        } else {
-                            // element.href = url_pasarela.replace('XXX', obj.cod_pay)
-                            element.href = obj.url_pay
-                        }
+                    brochure = document.querySelector('a.clickinfo')
+                    url_pay = document.querySelector('a.clickadmi')
 
-                    });
+                    brochure.href = obj.brochure
+                    name_ga.innerText = obj.program
+                    brochure.innerText = obj.brochure
+
+                    
+                    /*================================*/
+                    /*	SEGUIMIENTO DE LAS UTMS
+                    /*================================*/
+                    if (utmAux.includes('?utm')) {
+                        var utmAdded = utmAux.split('?utm_')[1]
+                        // element.href = url_pasarela.replace('XXX', obj.cod_pay) + '?utm_' + utmAdded
+                        url_pay.href = obj.url_pay + '?utm_' + utmAdded
+
+                    } else {
+                        // element.href = url_pasarela.replace('XXX', obj.cod_pay)
+                        url_pay.href = obj.url_pay
+                    }
+
 
                     throw BreakException;
 
